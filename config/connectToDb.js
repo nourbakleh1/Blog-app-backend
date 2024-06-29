79367a3765539c47083e5d0e271e29959516e583
nourbakleh1 <nourbakleh1@gmail.com>
2024-06-30 02:20:57 +0300

    update mongo

--- config/connectToDb.js
+++ config/connectToDb.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose");
 
 module.exports=async()=>{
     try{
-        await mongoose.connect(process.env.MONGO_CLOUD_URI);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("connected to mongoDB ^-^");
     }
     catch(error){
